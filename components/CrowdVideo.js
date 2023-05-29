ffefafd9718d6791feeeb1b4fd834a911a67b023
Edison <buzzedison@gmail.com>
2023-05-29 12:34:22 -0400

    edited the video size on mobile

--- components/CrowdVideo.js
+++ components/CrowdVideo.js
@@ -5,7 +5,7 @@ const VideoPlayer = ({ videoId }) => {
   
     return (
         <div className="pt-12 pb-12">
-      <div className="relative h-0 overflow-hidden max-w-screen-xl mx-auto w-3/4" style={{ paddingBottom: '56.25%', height: 0 }}>
+      <div className="relative h-0 overflow-hidden max-w-screen-xl mx-auto md:w-3/4 sm:w-full" style={{ paddingBottom: '56.25%', height: 0 }}>
         <iframe
           className="absolute top-0 left-0 w-full h-full"
           src={videoSrc}
